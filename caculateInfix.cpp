7cc856649ecaeff9c6892072cc557794f6da290b
Huynguyen <94187439+huyit251@users.noreply.github.com>
2022-09-24 14:11:27 +0700

    Update caculateInfix.cpp

--- caculateInfix.cpp
+++ caculateInfix.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 typedef struct node{
 	char data;
+	float data_f;
 	struct node* next;
 }Node;
 
@@ -12,20 +13,40 @@ typedef Node* Stack;
 void init(Stack &stack){
 	stack = NULL;
 }
+
 int isEmpty(Stack stack){
 	return stack == NULL;
 }
+
 Node* createNode(char data){
 	Node* tempNode = new Node();
 	tempNode->data = data;
 	tempNode->next = NULL;
 	return tempNode;
 }
+
+Node* createNode1(float data){
+	Node* tempNode = new Node();
+	tempNode->data_f = data;
+	tempNode->next = NULL;
+	return tempNode;
+}
+
+
 void push(Stack &stack , char data){
 	Node* tempNode = createNode(data);
-	tempNode->next = stack;
+	if(stack!=NULL)
+		tempNode->next = stack;
 	stack = tempNode;
 }
+
+void push1(Stack &stack , float data){
+	Node* tempNode = createNode1(data);
+	if(stack!=NULL)
+		tempNode->next = stack;
+	stack = tempNode;
+}
+
 char pop(Stack &stack){
 	char x ; 
 	if(stack != NULL){
@@ -36,6 +57,18 @@ char pop(Stack &stack){
 	}
 	return x;
 }
+
+float popFloat(Stack &stack){
+	float x ; 
+	if(stack != NULL){
+		Node* tempNode = stack ; 
+		x = tempNode->data_f;
+		stack = tempNode->next;
+		delete tempNode;
+	}
+	return x;
+}
+
 char peek(Stack stack){
 	char x;
 	if(stack != NULL){
@@ -55,79 +88,119 @@ int prec(char x){
 	}
 	return -1 ;
 }
-int isOperand(char number){
+void show(Stack stack){ 
+	if(stack != NULL){
+		printf("[");
+		while(stack != NULL){
+			printf(" %c ", stack->data);
+			stack = stack->next;
+		}
+		printf("]\n");
+	}
+}
+void showFloat(Stack stack){ 
+	if(stack != NULL){
+		printf("[");
+		while(stack != NULL){
+			printf(" %f ", stack->data_f);
+			stack = stack->next;
+		}
+		printf("]\n");
+	}
+}
+int isNumberics(char number){
 	return (number >='1' && number <= '9');
 }
-char calculateValue(char numberA , char x , char numberB){
+float calculateValue(float numberA , char x , float numberB){
 	float fResult = 0 ;
-	if(x = '+'){
-		fResult = (double)numberA + (double)numberB;
+	if(x == '+'){
+		fResult = numberA + numberB;
 	}
-	if(x = '-'){
-		fResult = (double)numberA - (double)numberB;
+	if(x == '-'){
+		fResult = numberA - numberB;
 	}
-	if(x = '*'){
-		fResult = (double)numberA * (double)numberB;
+	if(x == '*'){
+		fResult = numberA * numberB;
 	}
-	if(x = '/'){
-		fResult = (double)numberA / (double)numberB;
+	if(x == '/'){
+		fResult = numberA / numberB;
 	}
-	if(x = '^'){
+	if(x == '^'){
 		fResult = 1 ;
-		for(int i = 1 ; i < (int) (double)numberB;i++){
-			fResult = fResult*(double)numberA;
+		for(int i = 1 ; i < numberB;i++){
+			fResult = fResult*numberA;
 		}
 	}
-	char strResult = (char) fResult;
-	return strResult;
+//	char strResult = (char) fResult;
+	return fResult;
 }
 int calculateInfix(char infix[]){
 	Stack stackN = new Node();
+	init(stackN);
 	Stack stackO = new Node();
+	init(stackO);
+	
 	int size = strlen(infix);
 	float result = 0;
-	for(int i = 0 ; i < size ; i++){
-		if(isOperand(infix[i])){
-			push(stackN,infix[i]);
+	for(int i = 0 ; i < size ; i++){	
+		if(isNumberics(infix[i])){
+			float num = infix[i] - '0';
+			push1(stackN,num);
 		}
-//		else if(infix[i] == '('){
-//			push(stackN,infix[i]);
-//		}
-//		else if(infix[i] == ')'){
-//			while(!isEmpty(stackO) && peek(stackO) != '('){	
-//				char numberA , numberB , x;
-//				numberA = pop(stackN);
-//				numberB = pop(stackN);
-//				x = pop(stackO);
-//				push(stackN,calculateValue(numberA,x,numberB));		
-//			}
-//			if(peek(stackO) == ')')
-//				pop(stackO);
-//		}
+		else if(infix[i] == '('){
+			push(stackO,infix[i]);
+		}
+		else if(infix[i] == ')'){
+			while(!isEmpty(stackO) && peek(stackO) != '('){	
+				float numberA , numberB ;
+				char x;
+				numberA = popFloat(stackN);
+				numberB = popFloat(stackN);
+				x = pop(stackO);
+				float value = calculateValue(numberA,x,numberB);
+				push1(stackN,value);		
+//				showFloat(stackN);
+			}
+			if(peek(stackO) == '(')
+				pop(stackO);
+		}
 		else{
 			while(!isEmpty(stackO) && prec(infix[i]) <= prec(peek(stackO))){
-				char numberA , numberB , x;
-				numberA = pop(stackN);
-				numberB = pop(stackN);
+				float numberA , numberB;
+				char x;
+				numberA = popFloat(stackN);
+				numberB = popFloat(stackN);
 				x = pop(stackO);
-				push(stackN,calculateValue(numberA,x,numberB));
+				float value = calculateValue(numberA,x,numberB);
+//				printf("calculateValue: [%f] \n", value);
+				push1(stackN,value);
 			}
 			push(stackO,infix[i]);
 		}
+		
+//		show(stackO);
+//		showFloat(stackN);	
 	}
+	
 	while(stackO != NULL){
-		char numberA , numberB , x;
-		numberA = pop(stackN);
-		numberB = pop(stackN);
+		float numberA , numberB ;
+		char x;
+		numberA = popFloat(stackN);
+//		printf("numberA: %f \n", numberA);
+		numberB = popFloat(stackN);
+//		printf("numberB: %f \n", numberB);
 		x = pop(stackO);
-		push(stackN,calculateValue(numberA,x,numberB));
+		float value = calculateValue(numberA,x,numberB);
+//		printf("calculateValue: [%f] \n", value);
+		push1(stackN,value);		
+//		show(stackO);
+//		showFloat(stackN);	
 	}
-	char strResult ;
-	strResult = pop(stackN);
-	result = (double)strResult;
+	result= popFloat(stackN);
 	return result;
 }
 int main(){
-	char data[] = "1+2";
+	char data[] = "1+(2*3)";
+	
 	cout<<calculateInfix(data);
 }
